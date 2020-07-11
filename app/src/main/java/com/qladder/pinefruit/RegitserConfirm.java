0a382d24747d5ebcbd2e50572f8231c452de8e4f
selva <selvabhuvanesh@gmail.com>
2020-07-10 20:33:27 -0500

    Day changes at 6.2

---
+++ app/src/main/java/com/qladder/pinefruit/RegitserConfirm.java
@@ -0,0 +1,14 @@
+package com.qladder.pinefruit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegitserConfirm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regitser_confirm);
+    }
+}

8ae9dfc12ff798913e3c797d785f7f90b5d2ebf0
selva <selvabhuvanesh@gmail.com>
2020-07-22 22:13:43 -0500

    All functions working and added logos for all fields. validation to be done

--- app/src/main/java/com/qladder/pinefruit/SessionListActivity.java
+++ app/src/main/java/com/qladder/pinefruit/SessionListActivity.java
@@ -14,7 +14,8 @@ public class SessionListActivity extends AppCompatActivity {
     ListView mListView;
     //int image = 0;
     String sesionName[] = {"S_1","S_2","S_3","S_4","S_5","S_6","S_7","S_8","S_9","S_10","S_11"};
-    String sessionDetails[] = {"Detail_1","Detail_2","Detail_3","Detail_4","Detail_5","Detail_6","Detail_7","Detail_8","Detail_9","Detail_10","Detail_11"};
+    String sessionDetails[] = {"Detail_1. This line is detailed at length to test the folding feature that is set in the text view for multiple texts. this should not show more than 2 lines in mobile"
+            ,"Detail_2","Detail_3","Detail_4","Detail_5","Detail_6","Detail_7","Detail_8","Detail_9","Detail_10","Detail_11"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

622f969030489119e95646170da2b0c308856257
AarthiM <aarthimahes@gmail.com>
2020-07-29 22:02:03 +0530

    remove sops

--- app/src/main/java/com/qladder/pinefruit/SessionListActivity.java
+++ app/src/main/java/com/qladder/pinefruit/SessionListActivity.java
@@ -3,9 +3,14 @@ package com.qladder.pinefruit;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,31 +41,42 @@ public class SessionListActivity extends AppCompatActivity {
 
 
         mSessionInfoList = new ArrayList<>();
-       /* db = FirebaseDatabase.getInstance().getReference("Session");
+       db = FirebaseDatabase.getInstance().getReference("Session");
         db.addListenerForSingleValueEvent(new ValueEventListener() {
+    //    db.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                System.out.println("****************AM ENTERING READ *******************");
                 sessionInfo = new SessionInfo("","","asdasd","asdasda","Open");
 
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    sessionInfo.sessionID = ds.child("sessionID").getValue().toString();
-                    sessionInfo.sessionStatus = ds.child("sessionStatus").getValue().toString();
-                    sessionInfo.mfromtime = ds.child("mfromtime").getValue().toString();
-                    sessionInfo.mdate = ds.child("mdate").getValue().toString();
-                    mSessionInfoList.add(sessionInfo);
+               if (snapshot.exists()) {
+                   System.out.println("****************AM IN IF *******************");
+                   for (DataSnapshot ds : snapshot.getChildren()) {
+                       sessionInfo.sessionID = ds.child("sessionID").getValue().toString();
+                       System.out.println("****************AM IN FOR *******************" + ds.child("sessionID").getValue().toString());
+                       sessionInfo.sessionStatus = ds.child("sessionStatus").getValue().toString();
+                       sessionInfo.mfromtime = ds.child("mfromtime").getValue().toString();
+                       sessionInfo.mdate = ds.child("mdate").getValue().toString();
+                       mSessionInfoList.add(sessionInfo);
 
-                }
+                   }
+               } else {
+                   System.out.println("****************AM IN ELSE *******************");
+               }
 
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+                // ERRRRRRRROR
+                System.out.println("**************ONCANCALLED***********8");
             }
 
 
 
-        });*/
+        });
 
 
       int i=0;
